d679ffa8a659765a44f41b40a7c7ed62a1714312
Tero Parvinen <tero.parvinen@gmail.com>
2022-01-20 17:40:49 +0200

    Added square size ruler text calculation

--- module/rotating-square-template.js
+++ module/rotating-square-template.js
@@ -13,5 +13,20 @@ Hooks.on('init', () => {
         ];
 
         return new PIXI.Polygon(points);
-    }
+    };
+
+    const previous = CONFIG.MeasuredTemplate.objectClass.prototype._refreshRulerText;
+    CONFIG.MeasuredTemplate.objectClass.prototype._refreshRulerText = function() {
+        if ( this.data.t === "rect" ) {
+            const u = canvas.scene.data.gridUnits;
+
+            const d = Math.round(2 * this.data.distance * 10 / Math.sqrt(2)) / 10;
+            const text = `${d}${u}`;
+      
+            this.hud.ruler.text = text;
+            this.hud.ruler.position.set(this.ray.dx + 10, this.ray.dy + 5);
+        } else {
+            return previous.apply(this);
+        }
+    };
 });
